ffc483d1e6c1a959a6ed7b1909b8b2547760190e
caio-faustino <caioexfaustino.pro@gmail.com>
2022-10-28 12:14:47 -0300

    requisite 06

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 export default class SolarSystem extends Component {
   render() {
@@ -7,6 +9,17 @@ export default class SolarSystem extends Component {
       <div>
         <div data-testid="solar-system" />
         <Title headline="Planetas" />
+        <div>
+          {
+            planets.map((planet) => (
+              <PlanetCard
+                Key={planet.name}
+                planetName={planet.name}
+                planetImage={planet.image}
+              />
+            ))
+          }
+        </div>
       </div>
     );
   }
